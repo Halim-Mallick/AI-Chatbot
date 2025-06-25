68810691fb9e5bc00559a1932d58b1df4edd8865
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2025-06-25 14:53:41 +0600

    1.3.py

--- app.py
+++ app.py
@@ -80,6 +80,4 @@ def chat():
     return jsonify({"response": res})
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
